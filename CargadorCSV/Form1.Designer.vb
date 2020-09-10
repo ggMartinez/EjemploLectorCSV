74b5aa6f6c13a9cb2630b9cbf4220f205bf7d072
Gonzalo Martinez <gonzalo.martinez@trillonario.com>
2020-09-10 20:58:15 -0300

    Primer commit

---
+++ CargadorCSV/Form1.Designer.vb
@@ -0,0 +1,80 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnExplorar = New System.Windows.Forms.Button()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.btnProcesar = New System.Windows.Forms.Button()
+        Me.txtRuta = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'btnExplorar
+        '
+        Me.btnExplorar.Location = New System.Drawing.Point(42, 34)
+        Me.btnExplorar.Name = "btnExplorar"
+        Me.btnExplorar.Size = New System.Drawing.Size(75, 23)
+        Me.btnExplorar.TabIndex = 1
+        Me.btnExplorar.Text = "Explorar"
+        Me.btnExplorar.UseVisualStyleBackColor = True
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'btnProcesar
+        '
+        Me.btnProcesar.Location = New System.Drawing.Point(42, 63)
+        Me.btnProcesar.Name = "btnProcesar"
+        Me.btnProcesar.Size = New System.Drawing.Size(75, 23)
+        Me.btnProcesar.TabIndex = 2
+        Me.btnProcesar.Text = "Procesar"
+        Me.btnProcesar.UseVisualStyleBackColor = True
+        '
+        'txtRuta
+        '
+        Me.txtRuta.Enabled = False
+        Me.txtRuta.Location = New System.Drawing.Point(123, 35)
+        Me.txtRuta.Name = "txtRuta"
+        Me.txtRuta.Size = New System.Drawing.Size(445, 22)
+        Me.txtRuta.TabIndex = 3
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(609, 126)
+        Me.Controls.Add(Me.txtRuta)
+        Me.Controls.Add(Me.btnProcesar)
+        Me.Controls.Add(Me.btnExplorar)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnExplorar As Button
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents btnProcesar As Button
+    Friend WithEvents txtRuta As TextBox
+End Class
---
+++ CargadorCSV/Form1.vb
@@ -0,0 +1,37 @@
+﻿Public Class Form1
+    Private Sub btnExplorar_Click(sender As Object, e As EventArgs) Handles btnExplorar.Click
+        OpenFileDialog1.ShowDialog()
+        txtRuta.Text = OpenFileDialog1.FileName
+
+    End Sub
+
+    Private Sub btnProcesar_Click(sender As Object, e As EventArgs) Handles btnProcesar.Click
+        If txtRuta.Text = "" Then
+            MsgBox("Por favor, seleccione un archivo", MsgBoxStyle.Critical, "Error")
+        Else
+
+
+            Using archivo As New Microsoft.VisualBasic.
+                      FileIO.TextFieldParser(
+                        txtRuta.Text)
+                archivo.TextFieldType = FileIO.FieldType.Delimited
+                archivo.SetDelimiters(",")
+                Dim currentRow As String()
+                While Not archivo.EndOfData
+                    Try
+                        currentRow = archivo.ReadFields()
+                        Dim currentField As String
+                        Dim linea As String() = currentRow.ToArray()
+                        MsgBox(linea(1) + " " + linea(2) + " " + linea(3))
+
+                    Catch ex As Microsoft.VisualBasic.
+                                FileIO.MalformedLineException
+                        MsgBox("Line " & ex.Message &
+                        "is not valid and will be skipped.")
+                    End Try
+                End While
+            End Using
+        End If
+
+    End Sub
+End Class
